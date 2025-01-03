4b28136e620d4a8718952cc6f92dbe1a09e3c902
Euclides Marques <euclidesmarques270@gmail.com>
2025-01-02 21:03:26 -0300

    fix: Criação atualização dados cadastrais

--- LojaRoupas/Controllers/AreaClienteController.cs
+++ LojaRoupas/Controllers/AreaClienteController.cs
@@ -35,5 +35,39 @@ namespace LojaRoupas.Controllers
 
             return View(cliente);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Salvar(Cliente model)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var cliente = await _dbContext.Clientes.FirstOrDefaultAsync(c => c.Id == user.ClienteId);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            cliente.Nome = model.Nome ?? cliente.Nome;
+            cliente.CPF = model.CPF ?? cliente.CPF;
+            cliente.Email = model.Email ?? cliente.Email;
+            cliente.Celular = model.Celular ?? cliente.Celular;
+            cliente.CEP = model.CEP ?? cliente.CEP;
+            cliente.Logradouro = model.Logradouro ?? cliente.Logradouro;
+            cliente.Numero = model.Numero != 0 ? model.Numero : cliente.Numero;
+            cliente.Bairro = model.Bairro ?? cliente.Bairro;
+            cliente.Estado = model.Estado ?? cliente.Estado;
+            cliente.Cidade = model.Cidade ?? cliente.Cidade;
+
+            _dbContext.Clientes.Update(cliente);
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("MeusDados");
+        }
+
     }
 }
