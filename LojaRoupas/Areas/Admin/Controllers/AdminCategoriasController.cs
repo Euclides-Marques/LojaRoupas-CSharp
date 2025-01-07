fc9bd894d6ce9f27ba0872929471a84ff0291cb3
Euclides Marques <euclidesmarques270@gmail.com>
2025-01-07 20:46:36 -0300

    feature: Criação Método e View Create Categoria

--- LojaRoupas/Areas/Admin/Controllers/AdminCategoriasController.cs
+++ LojaRoupas/Areas/Admin/Controllers/AdminCategoriasController.cs
@@ -1,4 +1,5 @@
 ﻿using LojaRoupas.Context;
+using LojaRoupas.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -18,7 +19,27 @@ namespace LojaRoupas.Areas.Admin.Controllers
 
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Categorias.ToListAsync());
+            return View(await _context.Categorias.Where(c => c.Ativo == true).ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id, Nome, DataDeCadastro, Ativo")] Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoria);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(categoria);
         }
     }
 }
