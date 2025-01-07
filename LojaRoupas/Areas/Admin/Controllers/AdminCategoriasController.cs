48283e1a12e120b4cc3d9c1ccb55fd8c9184ad99
Euclides Marques <euclidesmarques270@gmail.com>
2025-01-07 19:54:07 -0300

    fix: Correção Entrar AdminCategorias

---
+++ LojaRoupas/Areas/Admin/Controllers/AdminCategoriasController.cs
@@ -0,0 +1,24 @@
+﻿using LojaRoupas.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaRoupas.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminCategoriasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AdminCategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categorias.ToListAsync());
+        }
+    }
+}
--- LojaRoupas/Program.cs
+++ LojaRoupas/Program.cs
@@ -74,12 +74,12 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
