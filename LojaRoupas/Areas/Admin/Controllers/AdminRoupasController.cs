1ffe3da06fc2bafc5b457d780a6f0f59c7a2c662
Euclides Marques <euclidesmarques270@gmail.com>
2025-01-16 17:56:38 -0300

    fix: Correções Filtro Roupas

--- LojaRoupas/Areas/Admin/Controllers/AdminRoupasController.cs
+++ LojaRoupas/Areas/Admin/Controllers/AdminRoupasController.cs
@@ -19,18 +19,9 @@ namespace LojaRoupas.Areas.Admin.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index(string filter, int pagindex = 1, string sort = "Nome")
+        public async Task<IActionResult> Index()
         {
-            var resultado = _context.Roupas.Include(r => r.Categoria).Include(r => r.Marca).AsQueryable();
-
-            if (!string.IsNullOrWhiteSpace(filter))
-            {
-                resultado = resultado.Where(r => r.Nome.Contains(filter));
-            }
-
-            var model = await PagingList.CreateAsync(resultado, 5, pagindex, sort, "Nome");
-            model.RouteValue = new RouteValueDictionary { { "filter", filter } };
-            return View(model);
+            return View(await _context.Roupas.ToListAsync());
         }
 
         [HttpGet]
--- LojaRoupas/Controllers/MainController.cs
+++ LojaRoupas/Controllers/MainController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LojaRoupas.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
 
 namespace LojaRoupas.Controllers
 {
     public class MainController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public MainController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(string filter, int pagindex = 1, string sort = "Nome")
+        {
+            var resultado = _context.Roupas.Include(r => r.Categoria).Include(r => r.Marca).AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(filter))
+            {
+                resultado = resultado.Where(r => r.Nome.Contains(filter));
+            }
+
+            var model = await PagingList.CreateAsync(resultado, 5, pagindex, sort, "Nome");
+            model.RouteValue = new RouteValueDictionary { { "filter", filter } };
+            return View(model);
         }
     }
 }
