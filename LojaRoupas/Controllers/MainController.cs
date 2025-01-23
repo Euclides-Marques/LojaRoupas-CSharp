fb551eadefe3b08352a84e2906178d3df4b6d29a
Euclides Marques <euclidesmarques270@gmail.com>
2025-01-23 19:01:02 -0300

    feature: Criação busca roupas por Categoria e Marca

--- LojaRoupas/Controllers/MainController.cs
+++ LojaRoupas/Controllers/MainController.cs
@@ -1,4 +1,5 @@
 ﻿using LojaRoupas.Context;
+using LojaRoupas.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ReflectionIT.Mvc.Paging;
@@ -14,7 +15,7 @@ namespace LojaRoupas.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index(string filter, int pagindex = 1, string sort = "Nome")
+        public async Task<IActionResult> Index(string filter, Guid? categoriaId, Guid? marcaId, int pagindex = 1, string sort = "Nome")
         {
             var resultado = _context.Roupas.Include(r => r.Categoria).Include(r => r.Marca).Where(r => r.Ativo == true).AsQueryable();
 
@@ -23,15 +24,27 @@ namespace LojaRoupas.Controllers
                 resultado = resultado.Where(r => r.Nome.Contains(filter));
             }
 
+            if (categoriaId.HasValue)
+            {
+                resultado = resultado.Where(r => r.CategoriaId == categoriaId);
+            }
+
+            if (marcaId.HasValue)
+            {
+                resultado = resultado.Where(r => r.MarcaId == marcaId);
+            }
+
             var model = await PagingList.CreateAsync(resultado, resultado.Count(), pagindex, sort, "Nome");
             ViewData["Filter"] = filter;
+            ViewData["CategoriaId"] = categoriaId;
+            ViewData["MarcaId"] = marcaId;
             return View(model);
         }
 
         [HttpGet("Detalhes/{id:guid}")]
         public async Task<IActionResult> Detalhes(Guid id)
         {
-            if(id == null) 
+            if(id == null)
             {
                 return NotFound();
             }
@@ -44,6 +57,28 @@ namespace LojaRoupas.Controllers
             }
 
             return View(roupa);
+        }
+
+        [HttpGet("Categoria/{categoriaId:guid}")]
+        public IActionResult GetRoupasByCategoria(Guid categoriaId)
+        {
+            if (categoriaId == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("Index", new { categoriaId });
+        }
+
+        [HttpGet("Marca/{marcaId:guid}")]
+        public IActionResult GetRoupasByMarca(Guid marcaId)
+        {
+            if(marcaId == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("Index", new { marcaId });
         }
 
         public IActionResult Carrinho()
